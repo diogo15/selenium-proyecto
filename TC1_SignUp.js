49682a439801128d10e25859fa05280b345fafa9
Inm0rt4LKoff33 <fabimv23@gmail.com>
2022-03-21 16:54:57 -0600

    changes_TC1

--- TC1_SignUp.js
+++ TC1_SignUp.js
@@ -4,7 +4,6 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
   let driver = await new Builder().forBrowser('chrome').build();
 
   await driver.get('http://intothezone.com');
-  await driver.sleep(1000);
 
   await driver.findElement(By.id('btn_carrito')).click();
   await driver.sleep(1000);
@@ -25,17 +24,25 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     for (let iterator = 0; iterator < inputs.length; iterator++) {
       inputs[iterator].sendKeys(data[iterator]);
 
-      driver.sleep(1000);
+      await driver.sleep(300);
       
       console.log(data[iterator]);
     }
 
+    await driver.sleep(3000);
     await driver.findElement(By.id('submit_signup_data')).click();
-    await driver.sleep(1000);
+
+    let response = await driver.wait(until.elementLocated(By.id('signup_response'))).getText();
+
+    if (await response == "user added") {
+      console.log("PASS " + response);
+    }else {
+      console.log("REJECTED " + response);
+    }
 
   } finally {
     
-    console.log("Datos Ingresados Correctamente");
+
     await driver.quit();
   
   }
