25fa7c591614e355e039ee51548663f3abaa7790
Inm0rt4LKoff33 <fabimv23@gmail.com>
2022-03-30 21:36:45 -0600

    changes-login

--- test/TC2_Login.js
+++ test/TC2_Login.js
@@ -2,26 +2,6 @@ const { Builder, Key,By, until } = require("selenium-webdriver");
 const login = require('../pageObjects/login');
 var assert = require("assert");
 
-<<<<<<< HEAD
-describe("Case 2 - Inicio de Sesión", function () {
-  it("Debería de iniciar sesión al usuario correctamente", async function () {
-    // Test Case 2
-    let driver = await new Builder().forBrowser("chrome").build();
-    //Ingreso a la pagina
-    await driver.get("http://intothezone.com/#/");
-    //Click al perfil
-    await driver.findElement(By.className("btn-login")).click();
-    //Rellenar campo de correo y contraseña
-    await driver
-      .findElement(By.xpath("//*[@id='app']/div[3]/div/form/input[1]"))
-      .sendKeys("asd@asd.com");
-    await driver
-      .findElement(By.xpath("//*[@id='app']/div[3]/div/form/input[2]"))
-      .sendKeys("123");
-    //Click en iniciar sesion
-    await driver.findElement(By.className("button green")).click();
-    await driver.close();
-=======
 // Test Case 2
 describe("Test Case 2", function () {
   it("Inicio Sesion", async function () {
@@ -43,6 +23,5 @@ describe("Test Case 2", function () {
       await driver.quit();
     }
 
->>>>>>> af8994a254acc56a60a2aaeea9f0a1ab687e4a6d
   });
 });
