922030d0881889b8449edb2e36c60d023a6dbcd7
Inm0rt4LKoff33 <fabimv23@gmail.com>
2022-04-07 13:15:19 -0600

    stage-change

--- test/TC12_Order.js
+++ test/TC12_Order.js
@@ -4,19 +4,29 @@ const compra = require("../pageObjects/compra");
 var assert = require("assert");
 var flag = new Boolean(false);
 var detalles = "/html/body/div/div[2]/div/div[2]/table[1]/tbody";
-let driver;
 
 describe("Case 12 - Detalle Pedidos", function () {
-  before(async function () {
-    driver = await new Builder().forBrowser("chrome").build();
-  });
-
-  it('Ingreso e inicio de sesión', async function () {
+  it("Ver datalles de los pedidos realizados", async function () {
+    let driver = await new Builder().forBrowser("chrome").build();
     //Precondiciones test case 12
     await driver.get("http://intothezone.com/#/tienda");
-
     //Inicio de sesion
     await login.login(driver, "asd@asd.com", "123");
     await driver.sleep(2500);
+    //Realizar compra
+    await compra.compra(driver, "product_4");
+    //Test case 12
+    //click historial de pedidos
+    await driver.findElement(By.className("active btn-login")).click();
+    // click ver detalles
+    await driver.wait(until.elementLocated(By.className("button"))).click();
+    flag = await driver
+      .wait(until.elementLocated(By.xpath(detalles)))
+      .isEnabled();
+    try {
+      assert.equal(flag, true);
+    } finally {
+      await driver.quit();
+    }
   });
 });
