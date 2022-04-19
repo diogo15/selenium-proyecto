2bab7566b1e94cf509d3bcd875cadb5534810c87
Diogo15 <luisdiegore@hotmail.com>
2022-04-18 20:00:22 -0600

    Test case 12 fix

--- pageObjects/compra.js
+++ pageObjects/compra.js
@@ -1,7 +1,7 @@
 const {By,until} = require("selenium-webdriver"); 
 
 async function compra(ventana,id){
-    await ventana.wait(until.elementLocated(By.id(id))).click();
+    await ventana.wait(until.elementLocated(By.id("product_1"))).click();
     await ventana.wait(until.elementLocated(By.className("button purple"))).click();
     await ventana.wait(until.elementLocated(By.className("btn-carrito"))).click();
     await ventana.wait(until.elementLocated(By.className("button green"))).click();
--- test/TC12_Order.js
+++ test/TC12_Order.js
@@ -1,6 +1,8 @@
 const { Builder, By, Key, until } = require("selenium-webdriver");
-const login = require("../pageObjects/login");
 const compra = require("../pageObjects/compra");
+const fillData = require('../pageObjects/fill_inputs');
+const params = require('../pageObjects/params');
+const login_data = require('../pageObjects/login_params');
 var assert = require("assert");
 var flag = new Boolean(false);
 var detalles = "/html/body/div/div[2]/div/div[2]/table[1]/tbody";
@@ -11,15 +13,20 @@ describe("Case 12 - Detalle Pedidos", function () {
     //Precondiciones test case 12
     await driver.get("http://intothezone.com/#/tienda");
     //Inicio de sesion
-    await login.login(driver, "asd@asd.com", "123");
+    await driver.findElement(By.className('btn-login')).click();
+    //await login.login(driver, params.defaultUser, params.defaultUserPass);
+    await fillData.fill_inputs(driver, login_data.data, params.login_submit);
     await driver.sleep(2500);
     //Realizar compra
     await compra.compra(driver, "product_4");
     //Test case 12
     //click historial de pedidos
     await driver.findElement(By.className("active btn-login")).click();
+    await driver.sleep(1000);
     // click ver detalles
     await driver.wait(until.elementLocated(By.className("button"))).click();
+    await driver.sleep(1000);
+
     flag = await driver
       .wait(until.elementLocated(By.xpath(detalles)))
       .isEnabled();
