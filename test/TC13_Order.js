9cb6960e260d19996ae90df588aabf169539c199
Diogo15 <luisdiegore@hotmail.com>
2022-04-18 20:04:31 -0600

    TC13, timeouts fixes

--- test/TC13_Order.js
+++ test/TC13_Order.js
@@ -1,6 +1,9 @@
 const { Builder, By, Key, until } = require("selenium-webdriver");
 const login = require("../pageObjects/login");
 const compra = require("../pageObjects/compra");
+const fillData = require('../pageObjects/fill_inputs');
+const params = require('../pageObjects/params');
+const login_data = require('../pageObjects/login_params');
 var assert = require("assert");
 var flag = new Boolean(false);
 
@@ -11,13 +14,16 @@ describe("Case 13 - Historial Pedidos", function () {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("http://intothezone.com/#/tienda");
     //Inicio de sesion
-    await login.login(driver, "asd@asd.com", "123");
+    await driver.findElement(By.className('btn-login')).click();
+    //await login.login(driver, params.defaultUser, params.defaultUserPass);
+    await fillData.fill_inputs(driver, login_data.data, params.login_submit);
     await driver.sleep(2500);
     //Realizar compra
-    await compra.compra(driver, "product_4");
+    await compra.compra(driver, "product_1");
     //Test case 13
     //click historial de pedidos
     await driver.findElement(By.id("btn_login_Active")).click();
+    await driver.sleep(2000);
     
     flag = await driver.wait(until.elementLocated(By.className('history'))).isEnabled();
 
